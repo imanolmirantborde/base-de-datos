1d85809018d19060a42888c5c3deffdec39fc49f
nicovelazquezz <112223468+nicovelazquezz@users.noreply.github.com>
2025-04-29 12:30:46 -0300

    ej 4 & 6

--- tp2/main.js
+++ tp2/main.js
@@ -14,7 +14,7 @@ db.empleados.insertMany([
 // Comprobar que se insertaron los documentos
 db.empleados.find();
 
-// Ejercicio 1
+{ /* ---------------------EJERCICIO 1 --------------------- */}
 // Actualizar la edad de uno de los empleados.
 db.empleados.updateOne(
     { nombre: "Imanol" },
@@ -31,3 +31,72 @@ db.empleados.deleteOne(
 
 // Comprobar que se eliminó
 db.empleados.find({ puesto: "Pasante" });
+
+{ /* ---------------------EJERCICIO 4 --------------------- */}
+
+// Para agregar la dirección a un empleado específico.
+
+db.empleados.updateOne(
+    { nombre: "Imanol" },
+    {
+        $set: {
+            direccion: {
+                calle: "Av. Colón 1234",
+                ciudad: "Bahía Blanca",
+                codigo_postal: "8000"
+            }
+        }
+    }
+);
+db.empleados.updateOne(
+    { nombre: "Jimena" },
+    {
+        $set: {
+            direccion: {
+                calle: "Av. Rivadavia 5678",
+                ciudad: "Buenos Aires",
+                codigo_postal: "1000"
+            }
+        }
+    }
+);
+
+// Para agregar la dirección a todos los empleados.
+
+db.empleados.updateMany(
+    {},
+    {
+        $set: {
+            direccion: {
+                calle: "11 de abril 475",
+                ciudad: "Bahía Blanca",
+                codigo_postal: "8000"
+            }
+        }
+    }
+);
+
+// Comprobar que se agregó el campo
+db.empleados.find();
+
+
+{ /* ---------------------EJERCICIO 6 --------------------- */}
+
+// Creación de la colección clientes
+db.createCollection("clientes");
+// Inserción de documentos en la colección 
+db.clientes.insertMany([
+    { nombre: "Juan", apellido: "Pérez", edad: 30 },
+    { nombre: "Ana", apellido: "Gómez", edad: 25 },
+    { nombre: "Luis", apellido: "Fernández", edad: 40 }
+]);
+
+// Crear indice compuesto
+
+db.clientes.createIndex(
+    { apellido: 1, nombre: 1 }
+);
+
+// Comprobaciones
+db.clientes.find();
+db.clientes.getIndexes();
