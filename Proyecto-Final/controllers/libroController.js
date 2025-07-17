e9bbe236dc68dade592a0e82dc91514786a8b950
unknown <imanol.mb@hotmail.com>
2025-07-17 16:03:54 -0300

    modificado

--- Proyecto-Final/controllers/libroController.js
+++ Proyecto-Final/controllers/libroController.js
@@ -3,9 +3,22 @@ const Libro = require('../models/Libro');
 
 exports.agregarLibro = async (datos) => {
   try {
+    const existe = await Libro.findOne({
+      $or: [
+        { isbn: datos.isbn },
+        { titulo: datos.titulo }
+      ]
+    });
+
+    if (existe) {
+      console.log('Ya existe un libro con el mismo título o ISBN.');
+      return;
+    }
+
     const libro = new Libro(datos);
     await libro.save();
     console.log('Libro agregado:', libro);
+
   } catch (error) {
     console.error('Error al agregar libro:', error.message);
   }
