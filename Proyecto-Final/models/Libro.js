3c9d6212540f9f5e3a3be465adf62bc2d130bc8c
belenburgos20 <belenburgos.dev@gmail.com>
2025-07-17 15:21:09 -0300

    Modelos

---
+++ Proyecto-Final/models/Libro.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const libroSchema = new mongoose.Schema({
+  titulo: {
+    type: String,
+    required: true,
+  },
+  autor: {
+    type: String,
+    required: true,
+  },
+  isbn: {
+    type: String,
+    required: true,
+    unique: true,  // ISBN debe ser único
+  },
+  genero: {
+    type: String,
+    required: true,
+  },
+  anioPublicacion: {
+    type: Number,
+    required: true,
+  },
+  copias: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  disponibles: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+});
+
+const Libro = mongoose.model('Libro', libroSchema);
+
+module.exports = Libro;
